070ac4de70285eda35a9907f6123e84a00d75112
DeepSea3474 <redbluealking@hotmail.com>
2026-01-19 16:20:41 +0200

    Fix: add AppProps typing in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
-import Navbar from "../components/Navbar";
-import "../styles/globals.css";
-<>
-  <Navbar />
-  <Component {...pageProps} />
-</>
+import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
 
