7c13c20d0064cd702ff2eb4a5fa0fd9401c1beaa
DeepSea3474 <redbluealking@hotmail.com>
2026-01-27 23:08:17 +0100

    Fix Tailwind PostCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./styles/**/*.{css}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
